d4945d87df726a4db3aba64be89151207ab52ac5
lukser69 <russergey2001@mail.ru>
2023-10-12 17:52:03 +0300

    feat: Созданы button и input

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -3,5 +3,10 @@
 
 <template>
   <div>
+    <nav>
+      <router-link to="/">Auth</router-link> |
+      <router-link to="/orders">Orders</router-link> |
+      <router-link to="/order-create">Order create</router-link>
+    </nav>
   </div>
 </template>
